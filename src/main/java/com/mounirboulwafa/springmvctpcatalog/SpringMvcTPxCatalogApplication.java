8019e535079870e56b581962b31646245c76cfd5
Mounit BOULWAFA <mounirboulwafa@gmail.com>
2019-03-24 12:22:39 +0100

    Initial commit

---
+++ src/main/java/com/mounirboulwafa/springmvctpcatalog/SpringMvcTPxCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.mounirboulwafa.springmvctpcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcTPxCatalogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcTPxCatalogApplication.class, args);
+    }
+
+}

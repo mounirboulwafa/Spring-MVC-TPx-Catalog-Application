bd1b57a88b4e38c6ce66bd1730183f6a4f79ff45
Mounit BOULWAFA <mounirboulwafa@gmail.com>
2019-03-24 21:26:53 +0100

    Delete Products

--- src/main/java/com/mounirboulwafa/springmvctpcatalog/SpringMvcTPxCatalogApplication.java
+++ src/main/java/com/mounirboulwafa/springmvctpcatalog/SpringMvcTPxCatalogApplication.java
@@ -15,10 +15,10 @@ public class SpringMvcTPxCatalogApplication {
         System.out.println("Project Works !!!");
         ApplicationContext ctx = SpringApplication.run(SpringMvcTPxCatalogApplication.class, args);
         ProduitRepository produitRepository = ctx.getBean(ProduitRepository.class);
-        produitRepository.save(new Produit("Dell XPS 15", 15000, 5));
-        produitRepository.save(new Produit("Lenovo 720", 10000, 3));
-        produitRepository.save(new Produit("Macbook Pro 15", 17000, 2));
-        produitRepository.save(new Produit("HP 562", 9000, 5));
+//        produitRepository.save(new Produit("Dell XPS 15", 15000, 5));
+//        produitRepository.save(new Produit("Lenovo 720", 10000, 3));
+//        produitRepository.save(new Produit("Macbook Pro 15", 17000, 2));
+//        produitRepository.save(new Produit("HP 562", 9000, 5));
 
         produitRepository.findAll().forEach(p -> System.out.println(p.getDesignation()));
 
--- src/main/java/com/mounirboulwafa/springmvctpcatalog/web/ProduitController.java
+++ src/main/java/com/mounirboulwafa/springmvctpcatalog/web/ProduitController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -20,20 +21,27 @@ public class ProduitController {
     @RequestMapping(value = "/index")
     public String index(Model model,
                         @RequestParam(name = "page", defaultValue = "0") int p,
-                        @RequestParam(name = "size", defaultValue = "7") int s) {
+                        @RequestParam(name = "size", defaultValue = "7") int s,
+                        @RequestParam(name = "motCle", defaultValue = "") String mc) {
 
 //        List<Produit> produits = produitRepository.findAll();     //Return all products in one page
 //        model.addAttribute("listProduits", produits);
 
-        Page<Produit> pageProduits = produitRepository.findAll(PageRequest.of(p, s));  //Results in Pages
+        Page<Produit> pageProduits = produitRepository.chercher("%" + mc + "%", PageRequest.of(p, s));  //Results in Pages
         model.addAttribute("listProduits", pageProduits.getContent());  //getContent() = return result products
 
         int[] pages = new int[pageProduits.getTotalPages()];
         model.addAttribute("pages", pages);
         model.addAttribute("size", s);
         model.addAttribute("pageCourante", p);
-
+        model.addAttribute("motCle", mc);
 
         return "produits";
+    }
+
+    @RequestMapping(value = "/delete", method = RequestMethod.GET)
+    public String delete(Long id, String motCle, int page, int size) {
+        produitRepository.deleteById(id);
+        return "redirect:/index?page=" + page + "&size=" + size + "&motCle=" + motCle;       //Delete product then redirect to index.html
     }
 }

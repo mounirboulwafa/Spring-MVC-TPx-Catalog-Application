4b3cd57b668f5c0b82d0ae768db948d287d1dd31
Mounit BOULWAFA <mounirboulwafa@gmail.com>
2019-03-30 20:59:20 +0100

    Add Layout

--- src/main/java/com/mounirboulwafa/springmvctpcatalog/entities/Produit.java
+++ src/main/java/com/mounirboulwafa/springmvctpcatalog/entities/Produit.java
@@ -3,6 +3,9 @@ package com.mounirboulwafa.springmvctpcatalog.entities;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 
 @Entity
@@ -10,7 +13,12 @@ public class Produit implements Serializable {
     @Id
     @GeneratedValue
     private Long id;
+
+    @NotNull
+    @Size(min = 4, max = 15)
     private String designation;
+
+    @DecimalMin("100")
     private double prix;
     private int quantite;
 
--- src/main/java/com/mounirboulwafa/springmvctpcatalog/web/ProduitController.java
+++ src/main/java/com/mounirboulwafa/springmvctpcatalog/web/ProduitController.java
@@ -7,10 +7,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -42,6 +44,24 @@ public class ProduitController {
     @RequestMapping(value = "/delete", method = RequestMethod.GET)
     public String delete(Long id, String motCle, int page, int size) {
         produitRepository.deleteById(id);
+
         return "redirect:/index?page=" + page + "&size=" + size + "&motCle=" + motCle;       //Delete product then redirect to index.html
+    }
+
+    @RequestMapping(value = "/form", method = RequestMethod.GET)
+    public String formProduit(Model model) {
+        model.addAttribute("produit", new Produit());
+
+        return "FormProduit";
+    }
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String save(Model model, @Valid Produit produit, BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            return "FormProduit";
+
+        produitRepository.save(produit);
+
+        return "Confirmation";
     }
 }
